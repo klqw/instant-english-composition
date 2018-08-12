51c549212f548eb8b64dad5aaa4bd8157002955d
klqw <komaki00334@gmail.com>
2018-08-12 12:19:43 +0900

    Heroku対応

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,7 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  'postgres://postgres:postgres@localhost/instant_english',
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/instant_english',
   {
     logging: true,
     operatorsAliases: false
